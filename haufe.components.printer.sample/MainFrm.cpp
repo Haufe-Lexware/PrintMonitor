801f4e9441bfe9823f308cca1b37f037d9c4ac8c
berndm <bernd.mutschler@outlook.com>
2019-02-06 12:17:21 +0100

    project cofiguration

--- haufe.components.printer.sample/MainFrm.cpp
+++ haufe.components.printer.sample/MainFrm.cpp
@@ -1,4 +1,19 @@
-// MainFrm.cpp : implementation of the CMainFrame class
+//	PrintMonitor for the Ghostscript Printer Driver
+//	Copyright (C) 2019  Haufe-Lexware
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU Affero General Public License as
+//	published by the Free Software Foundation, either version 3 of the
+//	License, or (at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU Affero General Public License for more details.
+//
+//	You should have received a copy of the GNU Affero General Public License
+//	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include "stdafx.h"
 #include "Sample.h"
 #include "MainFrm.h"
--- haufe.components.printer.sample/stdafx.h
+++ haufe.components.printer.sample/stdafx.h
@@ -1,6 +1,19 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently,
-// but are changed infrequently
+//	PrintMonitor for the Ghostscript Printer Driver
+//	Copyright (C) 2019  Haufe-Lexware
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU Affero General Public License as
+//	published by the Free Software Foundation, either version 3 of the
+//	License, or (at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU Affero General Public License for more details.
+//
+//	You should have received a copy of the GNU Affero General Public License
+//	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #pragma once
 #define VC_EXTRALEAN			// Exclude rarely-used stuff from Windows headers
 
